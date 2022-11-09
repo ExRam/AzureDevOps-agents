13500573e49470e514618d25ba0faa572ee11c04
Daniel Weber <dw@exram.de>
2022-11-09 21:01:31 +0100

    Update to .NET 7 stable SDK.

--- ubuntu20.04-dotnet/scripts/installers/dotnetcore-sdk.sh
+++ ubuntu20.04-dotnet/scripts/installers/dotnetcore-sdk.sh
@@ -9,5 +9,5 @@ wget https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 chmod +x ./dotnet-install.sh
 
 ./dotnet-install.sh --version 6.0.300
-./dotnet-install.sh --version 7.0.100-rc.1.22431.12
+./dotnet-install.sh --version 7.0.100
 
